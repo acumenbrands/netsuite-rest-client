44b51dcb8c7964768ec6a2b03a1e83c57a11d5ec
James Christie <jchristie@acumenholdings.com>
2012-01-30 16:43:15 -0600

    last of redone logic. now to get it working

--- lib/netsuite-rest-client.rb
+++ lib/netsuite-rest-client.rb
@@ -14,29 +14,39 @@ module Netsuite
                     "nlauth_email=#{login}," +
                     "nlauth_signature=#{password}," +
                     "nlauth_role=#{role_id}"
-      @headers = { :authorization => auth_string,
+
+      @headers = { :authorization  => auth_string,
                    :content_type   => "application/json" }
+
       @cookies = { "NS_VER" => "2011.2.0" }
     end
 
-    def get_saved_search(record_type, search_id, batch_size=20000)
-      url = BASE_URL  + "?script=10&deploy=1&record_type=#{record_type}&search_id=#{search_id}"
-      params = {
-                 "script" => 10,
-                 "deploy" => 1,
+    def get_saved_search(record_type, search_id, script_id, deploy_id, batch_size=20000, initial_id=0, timeout=-1)
+      params = { "script"      => 10,
+                 "deploy"      => 1,
                  "record_type" => record_type,
-                 "search_id" => search_id,
-               }
+                 "search_id"   => search_id }
 
-      $stderr.puts "Executing request via URL #{url} and headers #{@headers}"
+      $stderr.puts "Executing request via URL #{create_url(params)} and headers #{@headers}"
 
-      results_json = RestClient::Request.execute :method => :get,
-                                                 :url => url,
-                                                 :headers => @headers,
-                                                 :cookies => @cookies,
-                                                 :timeout => -1
+      results = Array.new
+      lower_bound = initial_id
+      upper_bound = initial_id + batch_size
+      while true
+        params['lower_bound'] = lower_bound
+        params['upper_bound'] = upper_bound
+        results_segment = JSON.parse(RestClient::Request.execute :method  => :get,
+                                                                 :url     => create_url(params),
+                                                                 :headers => @headers,
+                                                                 :cookies => @cookies,
+                                                                 :timeout => timeout)
+        results += results_segment
+        break if results_segment.empty? || results_segment.length < batch_size
+      end
+    end
 
-      JSON.parse(results_json)
+    def create_url(params)
+      BASE_URL + '?' + params.map { |key, value| "#{key}=#{value}" }.join('&')
     end
   end
 end
