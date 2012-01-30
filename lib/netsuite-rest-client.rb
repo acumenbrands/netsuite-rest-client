476dc24b2e758e152015433964fa31c309c2e6d5
James Christie <jchristie@acumenholdings.com>
2012-01-30 16:58:54 -0600

    fixes

--- lib/netsuite-rest-client.rb
+++ lib/netsuite-rest-client.rb
@@ -21,20 +21,18 @@ module Netsuite
       @cookies = { "NS_VER" => "2011.2.0" }
     end
 
-    def get_saved_search(record_type, search_id, script_id, deploy_id, batch_size=20000, initial_id=0, timeout=-1)
+    def get_saved_search(record_type, search_id, script_id, deploy_id, batch_size=20000, timeout=-1)
       params = { "script"      => 10,
                  "deploy"      => 1,
                  "record_type" => record_type,
-                 "search_id"   => search_id }
+                 "search_id"   => search_id,
+                 'lower_bound' => 0,
+                 'upper_bound' => batch_size }
 
       $stderr.puts "Executing request via URL #{create_url(params)} and headers #{@headers}"
 
       results = Array.new
-      lower_bound = initial_id
-      upper_bound = initial_id + batch_size
       while true
-        params['lower_bound'] = lower_bound
-        params['upper_bound'] = upper_bound
         results_segment = JSON.parse(RestClient::Request.execute :method  => :get,
                                                                  :url     => create_url(params),
                                                                  :headers => @headers,
@@ -42,7 +40,10 @@ module Netsuite
                                                                  :timeout => timeout)
         results += results_segment
         break if results_segment.empty? || results_segment.length < batch_size
+        params['lower_bound'] += batch_size + 1
+        params['upper_bound'] += batch_size
       end
+      results
     end
 
     def create_url(params)
