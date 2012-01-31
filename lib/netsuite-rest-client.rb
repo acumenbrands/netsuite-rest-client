81118ebd7cc7b019809333ed179808de2c8f2c1a
James Christie <jchristie@acumenholdings.com>
2012-01-31 13:24:38 -0600

    pagination almost working

--- lib/netsuite-rest-client.rb
+++ lib/netsuite-rest-client.rb
@@ -22,26 +22,29 @@ module Netsuite
     end
 
     def get_saved_search(record_type, search_id, script_id, deploy_id, batch_size=20000, timeout=-1)
-      params = { "script"      => 10,
-                 "deploy"      => 1,
-                 "record_type" => record_type,
-                 "search_id"   => search_id,
-                 'lower_bound' => 0,
-                 'upper_bound' => batch_size }
+      params = { 'script'      => 10,
+                 'deploy'      => 1,
+                 'record_type' => record_type,
+                 'search_id'   => search_id,
+                 'start_id'    => 0,
+                 'batch_size'  => batch_size }
 
       $stderr.puts "Executing request via URL #{create_url(params)} and headers #{@headers}"
 
       results = Array.new
       while true
+        puts "Fetching #{params['start_id']} and above"
         results_segment = JSON.parse(RestClient::Request.execute :method  => :get,
                                                                  :url     => create_url(params),
                                                                  :headers => @headers,
                                                                  :cookies => @cookies,
                                                                  :timeout => timeout)
-        results += results_segment
-        break if results_segment.empty? || results_segment.length < batch_size
-        params['lower_bound'] += batch_size + 1
-        params['upper_bound'] += batch_size
+        puts results_segment.first.count
+        puts results_segment.last
+        puts results == results_segment
+        results += results_segment.first
+        break if results_segment.empty?# || results_segment.length < batch_size
+        params['start_id'] = results_segment.last
       end
       results
     end
