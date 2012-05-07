c58888c7998afc259321406aae071f5d07fc762a
James Christie <james@james-Vostro-3550.(none)>
2012-05-07 16:16:24 -0500

    Increased default batch size and corrected a bug where id filter would not increment in the RESTlet

--- lib/netsuite-rest-client.rb
+++ lib/netsuite-rest-client.rb
@@ -139,7 +139,6 @@ module Netsuite
       end
 
       begin
-        puts params['batch_size']
         results_segment, params['start_id'] = *parse_json_result_from_rest(:get, params)
         results_segment.class == Array ? results += results_segment : raise("Search error: #{results_segment}")
         puts "Fetched #{results.count} records so far, querying from #{params['start_id']}..." if options[:verbose]
